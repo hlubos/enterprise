f9716a2106f5ecc25c7865d131e448224b9607c6
caojikui <caojikui@51yund.com>
2020-12-26 17:11:11 +0800

    save

--- pages/tabBar/home/home.js
+++ pages/tabBar/home/home.js
@@ -23,7 +23,7 @@ Page({
       }, {
         kind_id: 19,
         name: '俯卧撑',
-        path: '/pages/AI/TiaoSheng/TiaoSheng',
+        path: '/pages/AI/FuWoCheng/FuWoCheng',
         pic_url: 'https://ssl-pubpic.51yund.com/1010293806.jpg',
         video_id:33,
         cnt: 100
