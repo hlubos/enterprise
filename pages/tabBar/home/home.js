d14607135fb4a779f5baa4b99a7ff81c7aed483f
caojikui <caojikui@51yund.com>
2020-12-26 11:37:23 +0800

    动作列表

--- pages/tabBar/home/home.js
+++ pages/tabBar/home/home.js
@@ -9,58 +9,60 @@ Page({
         kind_id: 19,
         name: '跳绳',
         path: '/pages/AI/TiaoSheng/TiaoSheng',
-        pic_url: 'https://ssl-pubpic.51yund.com/1040424110.jpeg',
+        pic_url: 'https://17yd-common.51yund.com/ai_coach/icon_index_tiaoshen%403x.png',
+        video_id:45,
         cnt: 100
       },
       {
         kind_id: 19,
-        name: '跳绳',
+        name: '平板支撑',
         path: '/pages/AI/TiaoSheng/TiaoSheng',
-        pic_url: 'https://ssl-pubpic.51yund.com/1040424110.jpeg',
+        pic_url: 'https://ssl-pubpic.51yund.com/1010293873.jpg',
+        video_id:29,
         cnt: 100
       }, {
         kind_id: 19,
-        name: '跳绳',
+        name: '俯卧撑',
         path: '/pages/AI/TiaoSheng/TiaoSheng',
-        pic_url: 'https://ssl-pubpic.51yund.com/1040424110.jpeg',
+        pic_url: 'https://ssl-pubpic.51yund.com/1010293806.jpg',
+        video_id:33,
         cnt: 100
       }, {
         kind_id: 19,
-        name: '跳绳',
+        name: '深蹲',
         path: '/pages/AI/TiaoSheng/TiaoSheng',
-        pic_url: 'https://ssl-pubpic.51yund.com/1040424110.jpeg',
+        pic_url: 'https://ssl-pubpic.51yund.com/1010293895.jpg',
+        video_id:1,
         cnt: 100
       }, {
         kind_id: 19,
-        name: '跳绳',
+        name: '开合跳',
         path: '/pages/AI/TiaoSheng/TiaoSheng',
-        pic_url: 'https://ssl-pubpic.51yund.com/1040424110.jpeg',
+        pic_url: 'https://ssl-pubpic.51yund.com/1010293849.jpg',
+        video_id:2,
         cnt: 100
       }, {
         kind_id: 19,
-        name: '跳绳',
+        name: '仰卧起坐',
         path: '/pages/AI/TiaoSheng/TiaoSheng',
-        pic_url: 'https://ssl-pubpic.51yund.com/1040424110.jpeg',
+        pic_url: 'https://17yd-common.51yund.com/ai_coach/icon_index_yangwoqizuo%403x.png',
+        video_id:40,
         cnt: 100
       }, {
         kind_id: 19,
-        name: '跳绳',
+        name: '左体侧运动',
         path: '/pages/AI/TiaoSheng/TiaoSheng',
-        pic_url: 'https://ssl-pubpic.51yund.com/1040424110.jpeg',
+        pic_url: 'https://ssl-pubpic.51yund.com/1010294003.jpg',
+        video_id:4,
         cnt: 100
       }, {
         kind_id: 19,
-        name: '跳绳',
+        name: '右体侧运动',
         path: '/pages/AI/TiaoSheng/TiaoSheng',
-        pic_url: 'https://ssl-pubpic.51yund.com/1040424110.jpeg',
+        pic_url: 'https://ssl-pubpic.51yund.com/1010294056.jpg',
+        video_id:5,
         cnt: 100
-      }, {
-        kind_id: 19,
-        name: '跳绳',
-        path: '/pages/AI/TiaoSheng/TiaoSheng',
-        pic_url: 'https://ssl-pubpic.51yund.com/1040424110.jpeg',
-        cnt: 100
-      },
+      }
     ]
   },
 
