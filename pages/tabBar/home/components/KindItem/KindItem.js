7dcd1fec4ca7e2c154889b70be9de6cd036cc387
yangbo <yangbo@51yund.com>
2020-12-28 14:18:44 +0800

    feat: 更换测试链接

--- pages/tabBar/home/components/KindItem/KindItem.js
+++ pages/tabBar/home/components/KindItem/KindItem.js
@@ -24,7 +24,12 @@ Component({
     goSport () {
       let user_id = wx.getStorageSync('user_id')
       let info = this.data.info
-      if (info.video_id <= 0) return
+      if (info.video_id <= 0) {
+        return wx.showToast({
+          title: '敬请期待',
+          icon: 'none'
+        })
+      }
       if (user_id) {
         wx.navigateTo({
           url: `${info.path}?video_id=${info.video_id}&video_name=${info.name}`
