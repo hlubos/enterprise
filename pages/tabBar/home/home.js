2c70e0733de28e8ebc1ecabe15c3d54b66cb31fb
yangbo <yangbo@51yund.com>
2020-12-29 22:28:22 +0800

    feat: 更新首页动作数据

--- pages/tabBar/home/home.js
+++ pages/tabBar/home/home.js
@@ -1,4 +1,6 @@
 // pages/tabBar/home/home.js
+import api from '../../../server/home'
+
 Page({
 
   /**
@@ -12,7 +14,7 @@ Page({
       path: '/pages/AI/TiaoSheng/TiaoSheng',
       pic_url: 'https://17yd-common.51yund.com/ai_coach/icon_index_tiaoshen%403x.png',
       video_id: 45,
-      cnt: 100
+      cnt: 0
     },
     {
       kind_id: 15,
@@ -20,7 +22,7 @@ Page({
       path: '/pages/AI/FuWoCheng/FuWoCheng',
       pic_url: 'https://ssl-pubpic.51yund.com/1010293806.jpg',
       video_id: 33,
-      cnt: 100
+      cnt: 0
     },
     {
       kind_id: 22,
@@ -28,7 +30,7 @@ Page({
       path: '/pages/AI/ShenDun/ShenDun',
       pic_url: 'https://ssl-pubpic.51yund.com/1010293895.jpg',
       video_id: 1,
-      cnt: 100
+      cnt: 0
     },
     {
       kind_id: 17,
@@ -36,7 +38,7 @@ Page({
       path: '/pages/AI/YangWoQiZuo/YangWoQiZuo',
       pic_url: 'https://17yd-common.51yund.com/ai_coach/icon_index_yangwoqizuo%403x.png',
       video_id: 40,
-      cnt: 100
+      cnt: 0
     },
     {
       kind_id: 16,
@@ -44,7 +46,7 @@ Page({
       path: '/pages/AI/KaiHeTiao/KaiHeTiao',
       pic_url: 'https://ssl-pubpic.51yund.com/1010293849.jpg',
       video_id: 2,
-      cnt: 100
+      cnt: 0
     },
     {
       kind_id: -1,
@@ -61,7 +63,7 @@ Page({
       path: '/pages/AI/TiaoSheng/TiaoSheng',
       pic_url: 'https://17yd-common.51yund.com/ai_coach/icon_index_tiaoshen%403x.png',
       video_id: 45,
-      cnt: 100
+      cnt: 0
     },
     {
       kind_id: 19,
@@ -69,49 +71,49 @@ Page({
       path: '/pages/AI/ShuangBiZhiCheng/ShuangBiZhiCheng',
       pic_url: 'https://ssl-pubpic.51yund.com/1010293873.jpg',
       video_id: 29,
-      cnt: 100
+      cnt: 0
     }, {
       kind_id: 19,
       name: '俯卧撑',
       path: '/pages/AI/FuWoCheng/FuWoCheng',
       pic_url: 'https://ssl-pubpic.51yund.com/1010293806.jpg',
       video_id: 33,
-      cnt: 100
+      cnt: 0
     }, {
       kind_id: 19,
       name: '深蹲',
       path: '/pages/AI/ShenDun/ShenDun',
       pic_url: 'https://ssl-pubpic.51yund.com/1010293895.jpg',
       video_id: 1,
-      cnt: 100
+      cnt: 0
     }, {
       kind_id: 19,
       name: '开合跳',
       path: '/pages/AI/KaiHeTiao/KaiHeTiao',
       pic_url: 'https://ssl-pubpic.51yund.com/1010293849.jpg',
       video_id: 2,
-      cnt: 100
+      cnt: 0
     }, {
       kind_id: 19,
       name: '仰卧起坐',
       path: '/pages/AI/YangWoQiZuo/YangWoQiZuo',
       pic_url: 'https://17yd-common.51yund.com/ai_coach/icon_index_yangwoqizuo%403x.png',
       video_id: 40,
-      cnt: 100
+      cnt: 0
     }, {
       kind_id: 19,
       name: '左体侧运动',
       path: '/pages/AI/TiaoSheng/TiaoSheng',
       pic_url: 'https://ssl-pubpic.51yund.com/1010294003.jpg',
       video_id: 4,
-      cnt: 100
+      cnt: 0
     }, {
       kind_id: 19,
       name: '右体侧运动',
       path: '/pages/AI/TiaoSheng/TiaoSheng',
       pic_url: 'https://ssl-pubpic.51yund.com/1010294056.jpg',
       video_id: 5,
-      cnt: 100
+      cnt: 0
     }
     ]
   },
@@ -132,6 +134,7 @@ Page({
   },
 
   initData () {
+    this.updateKindList()
     let user_id = wx.getStorageSync('user_id')
     if (user_id) {
       this.setData({
@@ -142,6 +145,23 @@ Page({
   gotoRecords () {
     wx.navigateTo({
       url: '/pages/history/history'
+    })
+  },
+  updateKindList () {
+    api.getAISportActionList().then(res => {
+      let actionInfos = res.data.action_infos
+      this.formatKindList(actionInfos)
+    })
+  },
+  formatKindList (actionInfos) {
+    for (let i = 0; i < this.data.kindList.length; i++) {
+      const item = this.data.kindList[i];
+      let actions = actionInfos.filter(el => el.video_id == item['video_id'])
+      if (actions.length == 0) continue
+      this.data.kindList[i]['cnt'] = actions[0]['join_cnt']
+    }
+    this.setData({
+      kindList: this.data.kindList
     })
   }
 })
